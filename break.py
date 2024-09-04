24ad86e8e7fdb3630615830acb8e17c18d853508
nabila ahmed <sumayaad2020@gmail.com>
2024-09-04 18:48:10 +0600

    hi

--- break.py
+++ break.py
@@ -1,1 +1,9 @@
-print("hello world")
+a = input("enter a word:")
+
+for i in a:
+    if i == 'A' or i =="a":
+      print(f"A is not found iin {i}")
+      break
+  
+    else:
+      print(f"A is not found in {i}")
